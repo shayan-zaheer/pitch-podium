5e2acfd7faf67269fa07327e020ffdb287b2fa95
Shayan Zaheer <139066765+shayan-zaheer@users.noreply.github.com>
2024-12-19 04:26:03 +0500

    Finished PPR and added sanity write client

---
+++ components/Ping.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Ping() {
+  return (
+    <div className='relative'>
+        <div className='absolute -left-4 top-1'>
+            <span className='flex size-[11px]'>
+                <span className='absolute inline-flex h-full w-full animate-ping rounded-full bg-primary opacity-75'></span>
+            </span>
+        </div>
+    </div>
+  )
+}
+
+export default Ping
--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -30,3 +30,9 @@ export const STARTUP_BY_ID =
   image,
   pitch
 }`);
+
+export const STARTUP_VIEWS_QUERY = defineQuery(`
+  *[_type == "startup" && _id == $id][0]{
+    _id, views
+  }
+  `)
