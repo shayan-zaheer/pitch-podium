ead2aab2e8965bbcb20e951541ce996e21fbf758
Shayan Zaheer <139066765+shayan-zaheer@users.noreply.github.com>
2024-12-19 17:23:24 +0500

    User creation and authentication completed

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,6 +2,7 @@ import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 import { sanityFetch, SanityLive } from "@/sanity/lib/live";
+import { auth } from "@/auth";
 
 async function Home({
     searchParams,
@@ -10,6 +11,9 @@ async function Home({
 }) {
     const query = (await searchParams).query;
     const params = {search: query || null}
+
+    const session = await auth();
+    console.log(session?.id);
 
     const {data : posts} = await sanityFetch({query: STARTUPS_QUERY, params});
 
